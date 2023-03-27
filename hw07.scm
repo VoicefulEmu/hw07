61b24c85068a703a86495ff33525dce27149a44d
Ezra Lamoreaux <65139116+VoicefulEmu@users.noreply.github.com>
2023-03-27 11:22:25 -0600

    first commit

---
+++ hw07.scm
@@ -0,0 +1,23 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
+
+(define (ordered? s)
+  (cond 
+    ((null? s)             #t)
+    ((null? (cdr s))       #t)
+    ((<= (car s) (cadr s)) (ordered? (cdr s)))
+    (else                  #f)))
+
+(define (square x) (* x x))
+
+(define (pow base exp)
+  (cond 
+    ((=
+     exp
+     0)
+    1)
+  ((even? exp) (square (pow base (/ exp 2))))
+  ((odd? exp) (* base (pow base (- exp 1))))))
